812ea71215d8e1faab4dd5caa8e34c402f29f8bd
ssmolker <ssmolker@gmail.com>
2021-03-04 15:02:47 -0500

    portfolio

---
+++ Portfolio1/index.js
@@ -0,0 +1,12 @@
+const chalk = require('chalk');
+
+let quotes =[`"I have the power of God and anime on my side. AAAAAAAAAHHHHHHHHH"
+-child on Vine (circa 2015)`,
+`“Two bros, chillin’ in a hot tub, 5 feet apart cause they’re not gay.” -Anthony Padilla`,
+`“Oh hi, thanks for checking in I’m still a piece of garrbaagge.” -Bill Wurtz`]
+
+let phrase = "Thank you for using quote generator."
+
+let index = Math.floor(Math.random() * 3)
+
+console.log(chalk.magentaBright(quotes[index])+chalk.cyan('\n', phrase))
